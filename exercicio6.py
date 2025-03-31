8655053b0c95d24d984c07cbbbeae838765d1c28
Manoel Olimpio <olimpiommelo@gmail.com>
2025-03-31 17:02:50 -0300

    placar de times

---
+++ exercicio6.py
@@ -0,0 +1,8 @@
+sport=int(input("digite o numero de gols: \n"))
+santa=int(input("digite o numero de gols: \n"))
+if sport>santa:
+    print("Sport vencedor")
+else:
+    if santa>sport:
+        print("Santa vencedor")
+    else:print("Empate")
