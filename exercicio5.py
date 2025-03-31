f96a7a9dcb30bbd1927f9252db126af36b8a82a3
Manoel Olimpio <olimpiommelo@gmail.com>
2025-03-31 16:29:35 -0300

    media

---
+++ exercicio5.py
@@ -0,0 +1,8 @@
+nota1=float(input(f"Me diga a sua nota do primeiro trimestre!\n"))
+nota2=float(input(f"Agora a do segundo trimestre!\n"))
+nota3=float(input(f"Por ultimo a do terceiro!\n"))
+media=nota1+nota2+nota3/3
+if media>7:
+    print(f"Aluno Aprovado")
+else:
+    print(f"Aluno Reprovado")
