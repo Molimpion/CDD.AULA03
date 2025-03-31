1cf400a76f7b9b97fddbcbe34cad0b16ab4a494b
Manoel Olimpio <olimpiommelo@gmail.com>
2025-03-31 16:55:13 -0300

    mudanca

--- exercicio5.py
+++ exercicio5.py
@@ -3,8 +3,8 @@ nota2=float(input(f"Agora a do segundo trimestre!\n"))
 nota3=float(input(f"Por ultimo a do terceiro!\n"))
 media=(nota1+nota2+nota3)/3
 if media>7:
-    print(f"Aluno Aprovado")
+    print(f"Aluno Aprovado, media:{media:.2f}!")
 elif media >=4:
-    print(f"Aluno pode fazer recuperação")
+    print(f"Aluno pode fazer recuperação, media:{media:.2f}!")
 else:
-    print(f"Aluno reprovado sem chance de recuperação!")
+    print(f"Aluno reprovado sem chance de recuperação, media:{media:.2f}!")
