1025c71489d2356329bcb19c277f4c12ff45a6c8
Manoel Olimpio <olimpiommelo@gmail.com>
2025-03-31 16:47:31 -0300

    recuperacao

--- exercicio5.py
+++ exercicio5.py
@@ -1,8 +1,10 @@
 nota1=float(input(f"Me diga a sua nota do primeiro trimestre!\n"))
 nota2=float(input(f"Agora a do segundo trimestre!\n"))
 nota3=float(input(f"Por ultimo a do terceiro!\n"))
-media=nota1+nota2+nota3/3
+media=(nota1+nota2+nota3)/3
 if media>7:
     print(f"Aluno Aprovado")
+elif media >=4:
+    print(f"Aluno pode fazer recuperação")
 else:
-    print(f"Aluno Reprovado")
+    print(f"Aluno reprovado sem chance de recuperação!")
